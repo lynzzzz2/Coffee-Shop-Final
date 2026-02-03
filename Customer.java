1fb6db97090af09cefa282b34748336d51ee7555
mintacgabriel-lgtm <mintacgabriel@gmail.com>
2026-02-03 22:03:05 +0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -23,13 +23,4 @@ public Order(int orderNumber, Customer customer, String coffeeType) {
         this.coffee = new Coffee(coffeeType); // Created inside → Composition
     }
 
-    public String processOrder() {
-	return "Processing Order #" + orderNumber + "\n"
-                + customer.placeOrder() + "\n"
-                + coffee.brew();
-    }
-
-    public String identify() {
-        return "I am the Order class. I store the order number and manage the relationship between the customer and the coffee.";
-    }
-}
+   
