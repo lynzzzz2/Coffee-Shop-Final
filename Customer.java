ac976e49a5e8fc585d78bd46b43db157ec8b63d9
Christian Sabang <christiandalesabang@gmail.com>
2026-02-03 21:43:07 +0800

    Edit2

--- Customer.java
+++ Customer.java
@@ -24,3 +24,12 @@ public Order(int orderNumber, Customer customer, String coffeeType) {
     }
 
     public String processOrder() {
+	return "Processing Order #" + orderNumber + "\n"
+                + customer.placeOrder() + "\n"
+                + coffee.brew();
+    }
+
+    public String identify() {
+        return "I am the Order class. I store the order number and manage the relationship between the customer and the coffee.";
+    }
+}
