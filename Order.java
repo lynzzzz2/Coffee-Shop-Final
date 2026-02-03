627e30089706a6ba73bbd51e17d2fdec581da7d7
Lynzeee <lynzecabalona0@gmail.com>
2026-02-03 21:45:34 +0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -5,3 +5,9 @@ public class Order {
     public int orderNumber;
     public Customer customer;   // Aggregation
     public Coffee coffee;       // Composition
+
+ public Order(int orderNumber, Customer customer, String coffeeType) {
+        this.orderNumber = orderNumber;
+        this.customer = customer;
+        this.coffee = new Coffee(coffeeType); // Created inside → Composition
+    }
